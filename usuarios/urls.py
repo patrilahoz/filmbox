bfc5a8aa62b3aa91d04dfb4a22b71d3f189f47e4
patrilahoz <0259plahoz@e-itaca.es>
2025-12-21 20:49:56 +0100

    Registro y login

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("login/", views.login_view, name="login"),
+    path("registrarse/", views.register_view, name="register"),
     path("perfil/", views.perfil_user, name="perfil_user"),
 ]
+
+ # path("login/", views.login_view, name="login"),
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
-def login_view(request): 
-    return render(request, "usuarios/login.html")
+def register_view(request):
+    return render(request, "usuarios/registrarse.html")
 
 def perfil_user(request):
     return render(request, "usuarios/perfil_user.html")
+
+# Vista de inicio de sesión (login real)
+# def login_view(request):
+#    return render(request, "usuarios/login.html")
+#
